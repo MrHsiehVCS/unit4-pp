b5b2f9e8006a2d889b0f479eaef6bd7fbf08adb9
Eric Hsieh <89115321+MrHsiehVCS@users.noreply.github.com>
2023-10-17 19:33:19 +0000

    Basic Test Code in Main

--- src/main/java/u4pp/Main.java
+++ src/main/java/u4pp/Main.java
@@ -1,7 +1,20 @@
 package u4pp;
 
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to u4pp! Use this space to test your code as you go.\nGood luck! :)\n");
+        
+        // uncomment to manually test play method
+        /* 
+        Scanner myScanner = new Scanner(System.in);
+        RockPaperScissors.play(myScanner);
+        myScanner.close();
+        */
+
+        // uncomment and change to manually test Palindrome
+        // PalindromeTester.isPalindrome("test");
+
     }
 }
